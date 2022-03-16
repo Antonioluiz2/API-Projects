9efdaab8832717a6a022bd3d829f2460b013be43
Luiz <antonioluizti@hotmail.com>
2022-03-16 01:53:50 -0300

    commit2

---
+++ java/src/main/java/com/project/itexperts/java/controller/CardsController.java
@@ -0,0 +1,13 @@
+package com.project.itexperts.java.controller;
+
+
+public class CardsController {
+	private Long id;
+	private String name;
+	private Integer number;
+	private Integer accountCode;
+	private Integer digitCode;
+	private double limitBalance;
+	private String flag;
+	
+}
---
+++ java/src/main/java/com/project/itexperts/java/enums/Flag.java
@@ -0,0 +1,7 @@
+package com.project.itexperts.java.enums;
+
+public enum Flag {
+	 MASTERCARD,
+	    VISA,
+	    ELO
+}
